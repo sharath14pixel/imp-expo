b262d059f03a9f35c5fb6e13788ce899f512245a
sharu <sharathgowda14327@gmail.com>
2026-09-03 19:10:05 +0530

    chore: remove unused API keys from environment configuration

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -5,7 +5,7 @@ module.exports = {
   mongoUri: process.env.MONGODB_URI,
   corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:3000',
   resendApiKey: process.env.RESEND_API_KEY,
-  grokApiKey: process.env.GROK_API_KEY,
+
   cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
